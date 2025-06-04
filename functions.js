454287ff2896f9b986f67b620f80d65751919b17
amall-ts <amalllts0@gmail.com>
2025-06-04 23:19:15 +0530

    javascript

---
+++ functions.js
@@ -0,0 +1,25 @@
+ 
+
+//  function name(){
+//     console.log("my name is Amal");
+    
+//  }
+
+//  name()
+
+
+//  function deatails(name,age,place){
+//     console.log(`my name is ${name} iam ${age} years old iam from ${place}`);
+    
+//  }
+
+//  deatails("amal",22,"kerala")
+
+ function footballPlayers(name,country,club){
+      console.log(`${name} from ${country} in ${club}`);
+      
+ }
+
+ footballPlayers("neymar","brazil","santos FC")
+ footballPlayers("messi","argentina","inter miami")
+ footballPlayers("ronaldo","portugal","Al nassar")
--- ts4.js
+++ ts4.js
@@ -1,14 +1,139 @@
- let num = 7
+//  let num =" mn"
 
- if(num>0){
-    console.log("no is positive");
-    
- } else if( num<0){
-    console.log("no is negative");
-    
- }else{
-    console.log("no is zero");
-    
- }
+//  if(num>0){
+//     console.log("no is positive");
+
+//  } else if( num<0){
+//     console.log("no is negative");
+
+//  }else{
+//     console.log("no is zero");
+
+//  }
 
 
+//  if(num%2==0){
+//    console.log("num is even");
+
+//  }else{
+//    console.log("num is odd");
+
+//  }
+
+
+// if(num%2==0){
+//    console.log("num is even");
+
+// }else if(num%2 == !0)
+// {
+//    console.log("num is odd");
+
+// }
+
+// else{
+//    console.log("unexpected value");
+
+// }
+
+
+// let age = 20
+
+// if(age>=18){
+//    console.log("you can drive");
+
+
+// }else{
+//    console.log("you are minor");
+
+// }
+
+
+
+// let grade =90
+
+// if(grade>=90){
+//    console.log("grade A");
+
+// }else if(grade>=80){
+//    console.log("grade B");
+
+// }else if(grade>=70){
+//    console.log("grade C");
+
+// }else{
+//    console.log("you have failed");
+
+// }
+
+
+// let day = 6
+
+// switch (day) {
+//    case 1:
+//       console.log("monday");
+//       break;
+
+//    case 2:
+//       console.log("tuesday");
+//       break;
+//    case 3:
+//       console.log("wednesday");
+//       break;
+//    case 4:
+//       console.log("thursday");
+//       break;
+//    case 5:
+//       console.log("friday");
+//       break;
+//    default:
+//       console.log("invald number");
+
+// }
+
+
+
+// let no_of_wheels = 5
+
+// switch (no_of_wheels) {
+//    case 1:
+//       console.log("its just a wheel");
+//       break;
+//    case 2:
+//       console.log("its a bike");
+//       break;
+//    case 3:
+//       console.log("its a auto");
+//       break;
+//    case 4:
+//       console.log("its a car");
+//       break;
+//       default:
+//       console.log("its a lorry ");
+
+// }
+
+
+
+// for (i=1;i<=10;i++){
+//    console.log(i);
+   
+// }
+
+
+// let num = 2
+
+// let result = (num % 2 ==0) ? "even":"odd"
+// console.log(result);
+
+
+// let num = 3
+
+// let result = (num % 2 ==0) ? "even":"odd"
+
+// console.log(result);
+
+
+let num =-1
+
+let result = ( num > 0) ? "positvie" : "negative"
+console.log(result);
